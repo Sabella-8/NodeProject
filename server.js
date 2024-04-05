d95f78c1d9d6c1d0919f15f506451e4a29ebf9fc
Sabella-8 <gsabella549@gmail.com>
2024-04-05 18:39:46 +0100

    requesting query path

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 5500;
+const trainees = [
+  { name: "sabella", region: "north-west" },
+  {
+    name: "bella",
+    region: "north",
+  },
+  {
+    name: "fev",
+    region: "london",
+  },
+];
 app.get("/codeyourfuture", function (req, res) {
-  res.send("welcome to cyf");
+  console.log(req.query.region);
+  const query = req.query.region;
+  const filteredTrainees = trainees.filter((trainee) =>
+    trainee.region.includes(query)
+  );
+
+  res.send({ trainees: filteredTrainees });
 });
 app.get("/", function (req, res) {
   res.send("hello there");
